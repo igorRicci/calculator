4506f7179e1e500165ad6b63c41aa2e4d2bab0d1
igor ricci <assis.ricci@gmail.com>
2022-07-12 22:52:12 -0300

    calc skeleton ready

--- src/App.js
+++ src/App.js
@@ -12,6 +12,17 @@ function App() {
       <button>2</button>
       <button>3</button>
       <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   )
 }
