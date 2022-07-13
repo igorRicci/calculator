302bf574d492002c99bf828f67a7b428f933c19e
igor ricci <assis.ricci@gmail.com>
2022-07-12 23:22:56 -0300

    add style to output

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+
+import "./styles.css"
+
+
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">33 *</div>
+        <div className="current-operand">22</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
