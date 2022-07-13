1de37f04da9603a83267cda81d16967bb9d29636
igor ricci <assis.ricci@gmail.com>
2022-07-13 17:13:48 -0300

    operation button updates previous and current operands

--- src/App.js
+++ src/App.js
@@ -15,13 +15,33 @@ export const ACTIONS = {
 const reducer = (state, {type, payload}) => {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
-      if (payload.digit === "0" && state.currentOperand === "0") return state
-      if (payload.digit === "." && state.currentOperand.includes("."))  return state
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state
+      }
+
+      if (payload.digit === "." && state.currentOperand.includes(".")) {
+        return state
+      }
 
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`
       }
+    case ACTIONS.CHOOSE_OPERATION:
+      if (state.currentOperand == null && state.previousOperand == null) {
+        return state
+      }
+
+      if (state.previousOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null
+        }
+      }
+    case ACTIONS.CLEAR:
+      return {}
   }
 }
 
@@ -34,7 +54,7 @@ function App() {
         <div className="previous-operand">{previousOperand} {operation}</div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })} >AC</button>
       <button>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
